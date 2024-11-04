6354504472ea0b08ef540f51501f6e637aa20468
arunjoy128 <arunabrahamjoy@gmail.com>
2024-11-04 19:39:35 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -14,11 +15,11 @@ char size(int cms) {
 }
 
 int main() {
-    assert(size(36) == 'S');    // should pass
-    assert(size(38) == 'M');    // should fail
-    assert(size(40) == 'M');    // should pass
-    assert(size(42) == 'L');    // should fail
-    assert(size(43) == 'L');    // should pass
+    assert(size(37) == 'S');
+    assert(size(38) == 'S');
+    assert(size(40) == 'M');
+    assert(size(42) == 'M');
+    assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
